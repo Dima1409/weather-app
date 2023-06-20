98d58cfcc69f1eb2c0c93c3306850905f8ee5368
Dima <57713733+Dima1409@users.noreply.github.com>
2023-06-20 23:26:02 +0300

    add rain chance

--- src/components/NextDaysData/NextDaysData.tsx
+++ src/components/NextDaysData/NextDaysData.tsx
@@ -1,126 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { ISearchFiveDays } from "types/data";
-// import { searchFiveDays } from "Services/api";
-// import {
-//   LoadDate,
-//   LoadInfo,
-//   LoadTemp,
-//   SliderWrapper,
-// } from "../AdditionalData/AdditionalData.styled";
-// import { MinMaxTemp } from "./NextDays.styled";
-// import React from "react";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-
-// const NextDaysData: React.FC = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: false,
-//     speed: 1000,
-//     slidesToShow: 3,
-//     slidesToScroll: 2,
-//     arrows: false,
-//     autoplaySpeed: 3000,
-//   };
-//   const [searchFive, setSearchFive] = useState<ISearchFiveDays>();
-//   useEffect(() => {
-//     async function getAdditional() {
-//       try {
-//         const dataFiveDays: ISearchFiveDays = await searchFiveDays(
-//           localStorage.getItem("weather-value") || ""
-//         );
-//         setSearchFive(dataFiveDays);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//     getAdditional();
-//   }, []);
-//   // const urlIcon = process.env.REACT_APP_ICON_URL;
-//   const currentDate = new Date().toLocaleDateString().slice(0, 2);
-//   const dayFirst = searchFive?.list.filter((elem) => {
-//     const elemDate = elem.dt_txt.slice(8, 10);
-//     return Number(elemDate) === Number(currentDate) + 1;
-//   });
-//   const daySecond = searchFive?.list.filter((elem) => {
-//     const elemDate = elem.dt_txt.slice(8, 10);
-//     return Number(elemDate) === Number(currentDate) + 2;
-//   });
-//   const dayThird = searchFive?.list.filter((elem) => {
-//     const elemDate = elem.dt_txt.slice(8, 10);
-//     return Number(elemDate) === Number(currentDate) + 3;
-//   });
-//   const dayFourth = searchFive?.list.filter((elem) => {
-//     const elemDate = elem.dt_txt.slice(8, 10);
-//     return Number(elemDate) === Number(currentDate) + 4;
-//   });
-//   const dayFive = searchFive?.list.filter((elem) => {
-//     const elemDate = elem.dt_txt.slice(8, 10);
-//     return Number(elemDate) === Number(currentDate) + 5;
-//   });
-
-//   const temperatures = dayFirst?.map((item) => item.main.temp);
-//   const temperatures2 = daySecond?.map((item) => item.main.temp);
-//   const temperatures3 = dayThird?.map((item) => item.main.temp);
-//   const temperatures4 = dayFourth?.map((item) => item.main.temp);
-//   const temperatures5 = dayFive?.map((item) => item.main.temp);
-//   const minTemp = temperatures ? Math.min(...temperatures) : undefined;
-//   const maxTemp = temperatures ? Math.max(...temperatures) : undefined;
-//   const minTemp2 = temperatures2 ? Math.min(...temperatures2) : undefined;
-//   const maxTemp2 = temperatures2 ? Math.max(...temperatures2) : undefined;
-//   const minTemp3 = temperatures3 ? Math.min(...temperatures3) : undefined;
-//   const maxTemp3 = temperatures3 ? Math.max(...temperatures3) : undefined;
-//   const minTemp4 = temperatures4 ? Math.min(...temperatures4) : undefined;
-//   const maxTemp4 = temperatures4 ? Math.max(...temperatures4) : undefined;
-//   const minTemp5 = temperatures5 ? Math.min(...temperatures5) : undefined;
-//   const maxTemp5 = temperatures5 ? Math.max(...temperatures5) : undefined;
-//   return (
-//     <SliderWrapper {...settings}>
-//       <LoadInfo>
-//         <LoadDate>
-//           {new Date(new Date().getTime() + 86400000).toLocaleDateString()}
-//         </LoadDate>
-//         <LoadTemp><MinMaxTemp>Max:</MinMaxTemp> {maxTemp?.toFixed(1)} °C</LoadTemp>
-//         <LoadTemp><MinMaxTemp>Min:</MinMaxTemp> {minTemp?.toFixed(1)} °C</LoadTemp>
-//       </LoadInfo>
-
-//       <LoadInfo>
-//         <LoadDate>
-//           {new Date(new Date().getTime() + 86400000 * 2).toLocaleDateString()}
-//         </LoadDate>
-//         <LoadTemp><MinMaxTemp>Max:</MinMaxTemp> {maxTemp2?.toFixed(1)} °C</LoadTemp>
-//         <LoadTemp><MinMaxTemp>Min:</MinMaxTemp> {minTemp2?.toFixed(1)} °C</LoadTemp>
-//       </LoadInfo>
-
-//       <LoadInfo>
-//         <LoadDate>
-//           {new Date(new Date().getTime() + 86400000 * 3).toLocaleDateString()}
-//         </LoadDate>
-//         <LoadTemp><MinMaxTemp>Max:</MinMaxTemp> {maxTemp3?.toFixed(1)} °C</LoadTemp>
-//         <LoadTemp><MinMaxTemp>Min:</MinMaxTemp> {minTemp3?.toFixed(1)} °C</LoadTemp>
-//       </LoadInfo>
-
-//       <LoadInfo>
-//         <LoadDate>
-//           {new Date(new Date().getTime() + 86400000 * 4).toLocaleDateString()}
-//         </LoadDate>
-//         <LoadTemp><MinMaxTemp>Max:</MinMaxTemp> {maxTemp4?.toFixed(1)} °C</LoadTemp>
-//         <LoadTemp><MinMaxTemp>Min:</MinMaxTemp> {minTemp4?.toFixed(1)} °C</LoadTemp>
-//       </LoadInfo>
-
-//       <LoadInfo>
-//         <LoadDate>
-//           {new Date(new Date().getTime() + 86400000 * 5).toLocaleDateString()}
-//         </LoadDate>
-//         <LoadTemp><MinMaxTemp>Max:</MinMaxTemp> {maxTemp5?.toFixed(1)} °C</LoadTemp>
-//         <LoadTemp><MinMaxTemp>Min:</MinMaxTemp> {minTemp5?.toFixed(1)} °C</LoadTemp>
-//       </LoadInfo>
-//     </SliderWrapper>
-//   );
-// };
-
-// export default NextDaysData;
-
 import React, { useState, useEffect } from "react";
 import { ISearchFiveDays } from "types/data";
 import { searchFiveDays } from "Services/api";
@@ -176,7 +53,16 @@ const NextDaysData: React.FC = () => {
     const temperatures = filteredData?.map((item) => item.main.temp);
     const minTemp = temperatures ? Math.min(...temperatures) : undefined;
     const maxTemp = temperatures ? Math.max(...temperatures) : undefined;
+    const rain: string[] | undefined = filteredData?.map(
+      (item) => item.weather[0].description
+    );
+    const percentRain: number | undefined = rain?.filter((count) =>
+      count.includes("rain")
+    ).length;
+    const b: number =
+      percentRain && rain ? (percentRain * 100) / rain.length : 0;
 
+    // const urlIcon = process.env.REACT_APP_ICON_URL;
     return (
       <LoadInfo>
         <LoadDate>
@@ -191,6 +77,9 @@ const NextDaysData: React.FC = () => {
             )
           </span>
         </LoadDate>
+        <LoadTemp>
+           <MinMaxTemp>Chance of rain:</MinMaxTemp>{b.toFixed()} %
+        </LoadTemp>
         {temperatures ? (
           <>
             <LoadTemp>
