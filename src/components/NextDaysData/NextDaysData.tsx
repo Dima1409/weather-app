6e2410f343a20f3071d83e579012ea5024f37e5c
Dima <57713733+Dima1409@users.noreply.github.com>
2023-06-21 14:57:02 +0300

    updated fiveDaysData

--- src/components/NextDaysData/NextDaysData.tsx
+++ src/components/NextDaysData/NextDaysData.tsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactNode } from "react";
 import { ISearchFiveDays } from "types/data";
 import { searchFiveDays } from "Services/api";
 import {
   LoadDate,
   LoadInfo,
-  LoadTemp,
   SliderWrapper,
 } from "../AdditionalData/AdditionalData.styled";
-import { MinMaxTemp } from "./NextDays.styled";
+// import { MinMaxTemp } from "./NextDays.styled";
 import { SpinnerCircular } from "spinners-react";
+import { FaTemperatureLow, FaTemperatureHigh, FaWind } from "react-icons/fa";
 
 const NextDaysData: React.FC = () => {
   const settings = {
     dots: true,
     infinite: false,
     speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     arrows: false,
     autoplaySpeed: 3000,
   };
@@ -50,44 +50,87 @@ const NextDaysData: React.FC = () => {
 
   const renderWeatherInfo = (offset: number) => {
     const filteredData = getFilteredData(offset);
+    console.log(filteredData);
+
     const temperatures = filteredData?.map((item) => item.main.temp);
     const minTemp = temperatures ? Math.min(...temperatures) : undefined;
     const maxTemp = temperatures ? Math.max(...temperatures) : undefined;
+
     const rain: string[] | undefined = filteredData?.map(
       (item) => item.weather[0].description
     );
-    const percentRain: number | undefined = rain?.filter((count) =>
+    const countRain: number | undefined = rain?.filter((count) =>
       count.includes("rain")
     ).length;
-    const b: number =
-      percentRain && rain ? (percentRain * 100) / rain.length : 0;
+    const changeOfRain: number =
+      countRain && rain ? (countRain * 100) / rain.length : 0;
 
-    // const urlIcon = process.env.REACT_APP_ICON_URL;
+    const cloudiness: number[] | undefined = filteredData?.map(
+      (item) => item.clouds.all
+    );
+    const a: number | undefined = cloudiness?.reduce(
+      (acc, currentValue) => acc + currentValue,
+      0
+    );
+    const percentCloudiness: number | undefined =
+      a && cloudiness ? a / cloudiness.length : undefined;
+
+    const wind: number[] | undefined = filteredData?.map(
+      (item) => item.wind.speed
+    );
+    const minSpeed = wind ? Math.min(...wind) : undefined;
+    const maxSpeed = wind ? Math.max(...wind) : undefined;
+    const urlIcon = process.env.REACT_APP_ICON_URL;
+    const renderCloudIcon = (): ReactNode => {
+      const percentC: number | undefined = Number(percentCloudiness?.toFixed());
+      // const iconLink = `${urlIcon}${}.png`;
+      switch (true) {
+        case percentC <= 20:
+          return <img src={`${urlIcon}01d.png`} alt="clear sky" />;
+        case percentC <= 40:
+          return <img src={`${urlIcon}02d.png`} alt="few clouds" />;
+        case percentC <= 80:
+          return <img src={`${urlIcon}03d.png`} alt="clouds" />;
+        case percentC >= 81:
+          return <img src={`${urlIcon}04d.png`} alt="broken clouds" />;
+        default:
+          return null;
+      }
+    };
+
     return (
       <LoadInfo>
         <LoadDate>
-          {new Date(
-            currentDate.getTime() + 86400000 * (offset + 1)
-          ).toLocaleDateString()}
-          <span>
-            (
+          {new Date(currentDate.getTime() + 86400000 * (offset + 1))
+            .toLocaleDateString()
+            .slice(0, 5)}
+          <p>
             {new Date(
               new Date().getTime() + 86400000 * (offset + 1)
             ).toLocaleString("en-US", { weekday: "long" })}
-            )
-          </span>
+          </p>
         </LoadDate>
-        <LoadTemp>
-           <MinMaxTemp>Chance of rain:</MinMaxTemp>{b.toFixed()} %
-        </LoadTemp>
-        {temperatures ? (
+        <div>
+          <span>Rain:</span>
+          {changeOfRain.toFixed()} %
+        </div>
+        <div>{renderCloudIcon()}</div>
+        {temperatures && wind ? (
           <>
-            <LoadTemp>
-              <MinMaxTemp>Max:</MinMaxTemp> {maxTemp?.toFixed(1)} °C
-            </LoadTemp>
-            <LoadTemp>
-              <MinMaxTemp>Min:</MinMaxTemp> {minTemp?.toFixed(1)} °C
-            </LoadTemp>
+            <div>
+              <div>
+                <FaTemperatureLow size={15}></FaTemperatureLow>
+                {minTemp?.toFixed(1)} °C
+              </div>
+              <div>
+                <FaTemperatureHigh size={15}></FaTemperatureHigh>
+                {maxTemp?.toFixed(1)} °C
+              </div>
+            </div>
+            <div>
+              <FaWind size={15}></FaWind>
+              {minSpeed?.toFixed(1)}...{maxSpeed?.toFixed(1)} m/s
+            </div>
           </>
         ) : (
           <SpinnerCircular
