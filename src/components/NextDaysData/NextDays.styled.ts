6209cd3b2b733aced24755c0c68297e6c961b1f1
Dima <57713733+Dima1409@users.noreply.github.com>
2023-06-21 22:57:44 +0300

    edit fivedays data

--- src/components/NextDaysData/NextDays.styled.ts
+++ src/components/NextDaysData/NextDays.styled.ts
@@ -1,7 +1,58 @@
 import styled from "styled-components";
+import { theme } from "utils/theme";
+import { FaTemperatureLow, FaTemperatureHigh, FaWind } from "react-icons/fa";
+import { LoadDate } from "components/AdditionalData/AdditionalData.styled";
 
-const MinMaxTemp = styled.span`
-  font-size: 14px;
+const MainDate = styled(LoadDate)`
+  font-size: ${theme.fontSizes.small}px;
 `;
 
-export { MinMaxTemp };
+const DayOfWeek = styled(LoadDate)`
+  font-size: ${theme.fontSizes.medium}px;
+  margin: 0;
+`;
+
+const MinMaxTemp = styled.span`
+  font-size: ${theme.fontSizes.base}px;
+  color: ${(props) => props.theme.text};
+  margin-left: 4px;
+`;
+
+const Rain = styled.p`
+  font-size: ${theme.fontSizes.base}px;
+  margin: 0;
+  text-align: center;
+  color: ${(props) => props.theme.text};
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+`;
+const IconLow = styled(FaTemperatureLow)`
+  color: ${(props) => props.theme.text};
+`;
+const IconMax = styled(FaTemperatureHigh)`
+  color: ${(props) => props.theme.text};
+`;
+const IconWind = styled(FaWind)`
+  color: ${(props) => props.theme.text};
+`;
+const FewDaysWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+margin: 5px 0;
+`
+
+export {
+  MainDate,
+  DayOfWeek,
+  Rain,
+  MinMaxTemp,
+  Image,
+  IconLow,
+  IconMax,
+  IconWind,
+  FewDaysWrapper
+};
--- src/components/NextDaysData/NextDaysData.tsx
+++ src/components/NextDaysData/NextDaysData.tsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect, ReactNode } from "react";
 import { ISearchFiveDays } from "types/data";
 import { searchFiveDays } from "Services/api";
 import {
-  LoadDate,
   LoadInfo,
   SliderWrapper,
 } from "../AdditionalData/AdditionalData.styled";
-// import { MinMaxTemp } from "./NextDays.styled";
+import {
+  MainDate,
+  DayOfWeek,
+  Rain,
+  MinMaxTemp,
+  Image,
+  IconLow,
+  IconMax,
+  IconWind,
+  FewDaysWrapper,
+} from "./NextDays.styled";
 import { SpinnerCircular } from "spinners-react";
-import { FaTemperatureLow, FaTemperatureHigh, FaWind } from "react-icons/fa";
 
 const NextDaysData: React.FC = () => {
   const settings = {
@@ -23,9 +31,11 @@ const NextDaysData: React.FC = () => {
 
   const currentDate = new Date();
   const [searchFive, setSearchFive] = useState<ISearchFiveDays>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     async function getAdditional() {
+      setLoading(true);
       try {
         const dataFiveDays: ISearchFiveDays = await searchFiveDays(
           localStorage.getItem("weather-value") || ""
@@ -33,6 +43,8 @@ const NextDaysData: React.FC = () => {
         setSearchFive(dataFiveDays);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     }
     getAdditional();
@@ -50,7 +62,6 @@ const NextDaysData: React.FC = () => {
 
   const renderWeatherInfo = (offset: number) => {
     const filteredData = getFilteredData(offset);
-    console.log(filteredData);
 
     const temperatures = filteredData?.map((item) => item.main.temp);
     const minTemp = temperatures ? Math.min(...temperatures) : undefined;
@@ -68,12 +79,14 @@ const NextDaysData: React.FC = () => {
     const cloudiness: number[] | undefined = filteredData?.map(
       (item) => item.clouds.all
     );
-    const a: number | undefined = cloudiness?.reduce(
+    const accCloudiness: number | undefined = cloudiness?.reduce(
       (acc, currentValue) => acc + currentValue,
       0
     );
     const percentCloudiness: number | undefined =
-      a && cloudiness ? a / cloudiness.length : undefined;
+      accCloudiness && cloudiness
+        ? accCloudiness / cloudiness.length
+        : undefined;
 
     const wind: number[] | undefined = filteredData?.map(
       (item) => item.wind.speed
@@ -81,18 +94,18 @@ const NextDaysData: React.FC = () => {
     const minSpeed = wind ? Math.min(...wind) : undefined;
     const maxSpeed = wind ? Math.max(...wind) : undefined;
     const urlIcon = process.env.REACT_APP_ICON_URL;
+
     const renderCloudIcon = (): ReactNode => {
       const percentC: number | undefined = Number(percentCloudiness?.toFixed());
-      // const iconLink = `${urlIcon}${}.png`;
       switch (true) {
         case percentC <= 20:
-          return <img src={`${urlIcon}01d.png`} alt="clear sky" />;
+          return <Image src={`${urlIcon}01d.png`} alt="clear sky" />;
         case percentC <= 40:
-          return <img src={`${urlIcon}02d.png`} alt="few clouds" />;
+          return <Image src={`${urlIcon}02d.png`} alt="few clouds" />;
         case percentC <= 80:
-          return <img src={`${urlIcon}03d.png`} alt="clouds" />;
+          return <Image src={`${urlIcon}03d.png`} alt="clouds" />;
         case percentC >= 81:
-          return <img src={`${urlIcon}04d.png`} alt="broken clouds" />;
+          return <Image src={`${urlIcon}04d.png`} alt="broken clouds" />;
         default:
           return null;
       }
@@ -100,42 +113,44 @@ const NextDaysData: React.FC = () => {
 
     return (
       <LoadInfo>
-        <LoadDate>
-          {new Date(currentDate.getTime() + 86400000 * (offset + 1))
-            .toLocaleDateString()
-            .slice(0, 5)}
-          <p>
+        <div>
+          <MainDate>
+            {new Date(currentDate.getTime() + 86400000 * (offset + 1))
+              .toLocaleDateString()
+              .slice(0, 5)}
+          </MainDate>
+
+          <DayOfWeek>
             {new Date(
               new Date().getTime() + 86400000 * (offset + 1)
             ).toLocaleString("en-US", { weekday: "long" })}
-          </p>
-        </LoadDate>
-        <div>
-          <span>Rain:</span>
-          {changeOfRain.toFixed()} %
+          </DayOfWeek>
         </div>
+        <Rain>Rain: {changeOfRain.toFixed()} %</Rain>
         <div>{renderCloudIcon()}</div>
-        {temperatures && wind ? (
-          <>
-            <div>
-              <div>
-                <FaTemperatureLow size={15}></FaTemperatureLow>
-                {minTemp?.toFixed(1)} °C
-              </div>
-              <div>
-                <FaTemperatureHigh size={15}></FaTemperatureHigh>
-                {maxTemp?.toFixed(1)} °C
-              </div>
-            </div>
-            <div>
-              <FaWind size={15}></FaWind>
-              {minSpeed?.toFixed(1)}...{maxSpeed?.toFixed(1)} m/s
-            </div>
-          </>
-        ) : (
+        {loading ? (
           <SpinnerCircular
             style={{ display: "block", margin: "0 auto" }}
           ></SpinnerCircular>
+        ) : (
+          <>
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+              <FewDaysWrapper>
+                <IconLow size={20}></IconLow>
+                <MinMaxTemp>{minTemp?.toFixed(1)} °C</MinMaxTemp>
+              </FewDaysWrapper>
+              <FewDaysWrapper>
+                <IconMax size={20}></IconMax>
+                <MinMaxTemp>{maxTemp?.toFixed(1)} °C</MinMaxTemp>
+              </FewDaysWrapper>
+            </div>
+            <FewDaysWrapper>
+              <IconWind size={20}></IconWind>
+              <MinMaxTemp>
+                {minSpeed?.toFixed(1)}...{maxSpeed?.toFixed(1)} m/s
+              </MinMaxTemp>
+            </FewDaysWrapper>
+          </>
         )}
       </LoadInfo>
     );
